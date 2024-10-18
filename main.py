4f9eb46bc5e6065c4750bf35d933e3f1ed0a9ec3
vedanshipanchal <vedanshipanchal@gmail.com>
2024-10-18 17:26:01 +0100

    Weather condition

---
+++ main.py
@@ -0,0 +1,7 @@
+def weather():
+    print("I like it when its",good)
+    print("i hate it when its",bad)
+
+good = input("Enter the weather you love:")
+bad = input("Enter the weather you hate:")
+weather()
